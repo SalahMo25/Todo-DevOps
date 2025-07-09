79b39d02c6848f904667896aacef0860b3efde65
salahmohamed <salahmohamed906_sd@fcis.bsu.edu.eg>
2025-07-09 04:29:54 +0000

    Updated 0001_initial.py

--- todo/todo/migrations/0001_initial.py
+++ todo/todo/migrations/0001_initial.py
@@ -15,7 +15,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Todo',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('title', models.CharField(max_length=100)),
                 ('details', models.TextField()),
                 ('date', models.DateTimeField(default=django.utils.timezone.now)),
