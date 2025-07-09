f8e5be931d0e39043b8e704c0aa3f7a979cab5fa
salahmohamed <salahmohamed906_sd@fcis.bsu.edu.eg>
2025-07-09 04:34:19 +0000

    Updated forms.py

--- todo/todo/forms.py
+++ todo/todo/forms.py
@@ -7,6 +7,7 @@ class TodoForm(forms.ModelForm):
         model = Todo
         fields = '__all__'
 
+
     def __init__(self, *args, **kwargs):
         super(TodoForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
