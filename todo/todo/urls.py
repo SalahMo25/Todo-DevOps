ff0cffa8ba52217f2e32b5b16cb32fc221892121
salahmohamed <salahmohamed906_sd@fcis.bsu.edu.eg>
2025-07-09 04:40:49 +0000

    Updated urls.py

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='todo', permanent=False)),
-    path('todo', views.todo , name='todo'  ),
-    path('delete/<int:id>', views.remove , name='delete'  ),
+    path('todo', views.todo, name='todo'),
+    path('delete/<int:id>', views.remove, name='delete'),
 ]
